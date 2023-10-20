8351e4b3499537c22148f6feceed8e2fa7673e52
Saúl Santana <sausantana@aidacanarias.com>
2023-10-20 11:53:22 +0100

    Sincronización de git

---
+++ StringCalculator/Controller/StringCalculatorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StringCalculator.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class StringCalculatorController : ControllerBase
+    {
+        StringCalculatorClass calc = new StringCalculatorClass();
+
+        // GET api/<StringCalculatorController>/5
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            return calc.add(id).ToString(); ;
+        }
+
+    }
+}
+
--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -1,9 +1,30 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace StringCalculator {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World, hello repo!!");
-        }
-    }
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
