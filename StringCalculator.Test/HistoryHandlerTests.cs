286cea3941054c9b7ce7077f14a4d661efcb9272
Saúl Santana <sausantana@aidacanarias.com>
2023-10-25 12:23:59 +0100

    red:  return_expected_string_calculator_result

---
+++ StringCalculator.Test/HistoryHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using StringCalculator.Persistance;
+
+
+namespace HisoryControllerHandler.Test
+{
+    internal class HistoryHandlerShould
+    {
+
+        private StringCalculatorHistoryHandler handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            handler = new StringCalculatorHistoryHandler();
+        }
+
+
+        [Test]
+        public void return_expected_string_calculator_result()
+        {
+            var res = handler.HandleRequest("1,2,4");
+
+            res.Should().Be(7);
+        }
+
+        [Test]
+        public void write_given_data_on_correct_format()
+        {
+
+        }
+    }
+}
---
+++ StringCalculator/Persistance/StringCalculatorHistoryHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace StringCalculator.Persistance
+{
+    public class StringCalculatorHistoryHandler
+    {
+        private HistoryStorer storer;
+        public StringCalculatorHistoryHandler(string path = "history.txt")
+        {
+            this.storer = new HistoryStorer(path);
+        }
+
+        public int HandleRequest(string id) {
+            throw new NotImplementedException();
+        }
+    }
+}
